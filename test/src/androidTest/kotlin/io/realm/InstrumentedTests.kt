35e30dfaedfdd4236c838f75e42d8c299d24536d
Nabil Hachicha <nabil.hachicha@mongodb.com>
2020-09-29 00:43:27 +0100

    Fixing test: removing duplicate test class for JVM (test in common will run per target)

--- test/src/androidTest/kotlin/io/realm/InstrumentedTests.kt
+++ test/src/androidTest/kotlin/io/realm/InstrumentedTests.kt
@@ -1,10 +1,9 @@
 package io.realm
 
 import android.support.test.runner.AndroidJUnit4
-import junit.framework.TestCase.*
+import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
-import test.Sample
 
 
 @RunWith(AndroidJUnit4::class)
@@ -15,13 +14,4 @@ class InstrumentedTests {
     fun contextIsNotNull() {
         assertNotNull(RealmInitProvider.applicationContext)
     }
-
-    // Smoke test of compiler plugin
-    @Test
-    fun testCompilerPlugin() {
-        val p = Sample()
-        p.name = "Realm"
-        assertEquals("Hello Realm", p.name)
-    }
-
 }
