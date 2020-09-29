4e155002086ddf19d1f04461948690d690cc752d
Claus Rørbech <claus.rorbech@mongodb.com>
2020-09-29 12:57:19 +0200

    Extract more conventions into input files wrapper

--- realm-compiler-plugin/compiler-plugin/src/test/kotlin/io/realm/compiler/GenerationExtensionTest.kt
+++ realm-compiler-plugin/compiler-plugin/src/test/kotlin/io/realm/compiler/GenerationExtensionTest.kt
@@ -14,6 +14,16 @@ import kotlin.test.assertTrue
 
 class GenerationExtensionTest {
 
+    /**
+     * Wrapping conventions around test cases.
+     *
+     * Convention is that the subfolders of `directory` contains:
+     * - `input` - Kotlin input files
+     * - `output` - IR dump from compilation
+     * - `expected` - Expected IR dump
+     *
+     * @param directory Directory containing test case files.
+     */
     class Files(val directory: String) {
         val fileMap: Map<String, File>
         init {
@@ -23,28 +33,23 @@ class GenerationExtensionTest {
                     .filter{ !it.isDirectory }
                     .map { it.relativeTo(base).path to it}.toMap()
         }
-        fun expectedDir() = listOf("src", "test", "resources", directory, "output").joinToString(separator = File.separator)
+        fun expectedDir() = listOf("src", "test", "resources", directory, "expected").joinToString(separator = File.separator)
         fun outputDir() = listOf("src", "test", "resources", directory, "output").joinToString(separator = File.separator)
+
+        fun assertOutput() {
+            stripInputPath(File("${outputDir()}/00_ValidateIrBeforeLowering.ir"), fileMap)
+            assertEquals(
+                    File("${expectedDir()}/00_ValidateIrBeforeLowering.ir").readText(),
+                    File("${outputDir()}/00_ValidateIrBeforeLowering.ir").readText()
+            )
+        }
     }
 
     @Test
     fun transform() {
-        val plugins: List<Registrar> = listOf(Registrar())
-
         val inputs = Files("/sample")
 
-        val result = KotlinCompilation().apply {
-            sources = inputs.fileMap.values.map { SourceFile.fromPath(it)}
-            useIR = true
-            messageOutputStream = System.out
-            compilerPlugins = plugins
-            inheritClassPath = true
-            kotlincArguments = listOf(
-                    "-Xuse-ir",
-                    "-Xdump-directory=${inputs.outputDir()}",
-                    "-Xphases-to-dump-after=ValidateIrBeforeLowering"
-            )
-        }.compile()
+        val result = compile(inputs)
 
         assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
 
@@ -58,20 +63,32 @@ class GenerationExtensionTest {
             }
         }
 
-        stripInputPath(File("${inputs.outputDir()}/00_ValidateIrBeforeLowering.ir"), inputs.fileMap)
+        inputs.assertOutput()
 
-        assertEquals(
-                File("${inputs.expectedDir()}/00_ValidateIrBeforeLowering.ir").readText(),
-                File("${inputs.outputDir()}/00_ValidateIrBeforeLowering.ir").readText(),
-        )
     }
 
-    private fun stripInputPath(file: File, map: Map<String, File>) {
-        file.writeText(
-            map.entries.fold(file.readText()) { text, (name, file) ->
-                text.replace(file.path, name)
-            }
-        )
+    private fun compile(inputs: Files, plugins: List<Registrar> = listOf(Registrar())): KotlinCompilation.Result =
+        KotlinCompilation().apply {
+            sources = inputs.fileMap.values.map { SourceFile.fromPath(it)}
+            useIR = true
+            messageOutputStream = System.out
+            compilerPlugins = plugins
+            inheritClassPath = true
+            kotlincArguments = listOf(
+                    "-Xuse-ir",
+                    "-Xdump-directory=${inputs.outputDir()}",
+                    "-Xphases-to-dump-after=ValidateIrBeforeLowering"
+            )
+        }.compile()
+
+    companion object {
+        private fun stripInputPath(file: File, map: Map<String, File>) {
+            file.writeText(
+                    map.entries.fold(file.readText()) { text, (name, file) ->
+                        text.replace(file.path, name)
+                    }
+            )
+        }
     }
 
 }
