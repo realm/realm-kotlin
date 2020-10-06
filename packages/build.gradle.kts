d06836123f0e7ba5f7b33200b0f4387aa2a625f9
Christian Melchior <christian@ilios.dk>
2020-10-06 09:28:16 +0200

    Enable ktlint

--- packages/build.gradle.kts
+++ packages/build.gradle.kts
@@ -2,9 +2,8 @@ plugins {
     kotlin("multiplatform") version Versions.kotlin apply false
     kotlin("jvm") version Versions.kotlin apply false
     kotlin("kapt") version Versions.kotlin apply false
-
+    id("org.jlleitschuh.gradle.ktlint") version Versions.ktlintPlugin
     id("com.android.library") apply false
-
     `java-gradle-plugin`
 }
 
@@ -19,5 +18,28 @@ allprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        // TODO: Figure out how to extract this so it can be shared between all projects we run ktlint on
+        version.set(Versions.ktlintVersion)
+        debug.set(false)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        reporters {
+            // Human readable output
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
+            // Checkstyle compatible output for CI
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+        ignoreFailures.set(false)
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
     }
 }
