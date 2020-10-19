152827d40a245ce54ae4fb5ad2c2ddfd13432fa7
Claus Rørbech <claus.rorbech@mongodb.com>
2020-10-19 09:06:15 +0200

    Remove legazy import

--- packages/library/src/commonMain/kotlin/io/realm/RealmModel.kt
+++ packages/library/src/commonMain/kotlin/io/realm/RealmModel.kt
@@ -2,7 +2,6 @@ package io.realm
 
 import io.realm.runtimeapi.NativePointer
 
-expect class BindingPointer : NativePointer
 // use type alias to map CPointer<realm_object_t>? between Android and iOS
 abstract class RealmModel(var isManaged: Boolean, var objectPointer: NativePointer?, var tableName: String?, realm: Realm?) {
     constructor() : this (false, null, null, null)
