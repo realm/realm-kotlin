9473df77251cada0feabd97dbcbf63f00b799718
Nabil Hachicha <nabil.hachicha@mongodb.com>
2020-10-21 11:08:52 +0100

    Fixing ios test

--- packages/cinterop/src/iosTest/kotlin/io/realm/CinteropTest.kt
+++ packages/cinterop/src/iosTest/kotlin/io/realm/CinteropTest.kt
@@ -2,6 +2,7 @@ package io.realm
 
 import kotlinx.cinterop.ByteVarOf
 import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.CPointerVarOf
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.cValue
@@ -12,6 +13,7 @@ import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.nativeHeap
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.toKString
+import kotlinx.cinterop.useContents
 import realm_wrapper.RLM_CLASS_NORMAL
 import realm_wrapper.RLM_COLLECTION_TYPE_NONE
 import realm_wrapper.RLM_PROPERTY_NORMAL
@@ -22,13 +24,23 @@ import realm_wrapper.realm_config_set_path
 import realm_wrapper.realm_config_set_schema
 import realm_wrapper.realm_config_set_schema_mode
 import realm_wrapper.realm_config_set_schema_version
+import realm_wrapper.realm_error_t
+import realm_wrapper.realm_get_last_error
 import realm_wrapper.realm_get_library_version
+import realm_wrapper.realm_get_schema
+import realm_wrapper.realm_open
 import realm_wrapper.realm_property_info_t
 import realm_wrapper.realm_schema_mode_e
 import realm_wrapper.realm_schema_new
+import realm_wrapper.realm_schema_t
 import realm_wrapper.realm_schema_validate
 import realm_wrapper.realm_string_t
+import realm_wrapper.realm_t
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 @ExperimentalUnsignedTypes
@@ -61,23 +73,36 @@ class CinteropTest {
             }
 
 //            val classes: kotlinx.cinterop.CValuesRef<realm_class_info_t>? = allocArray<realm_class_info_t>(1)
-            val classes = allocArray<realm_class_info_t>(1)
+            val classes: CPointer<realm_class_info_t> = allocArray(1)
             classes[0].apply {
                 name.data = "foo".cstr.ptr
                 name.size = 3.toULong()
-
+//
                 primary_key.data = "".cstr.ptr
                 primary_key.size = 0.toULong()
 
-                num_properties = 3.toULong()
+                num_properties = 1.toULong()
                 num_computed_properties = 0.toULong()
 
 //                key.table_key = 123.toUInt() Unused when defining the schema.
 
                 flags = RLM_CLASS_NORMAL.toInt()
             }
-            val classProperties = cValuesOf(prop_1_1.ptr).ptr
+            val classProperties: CPointer  <CPointerVarOf<CPointer<realm_property_info_t /* = realm_wrapper.realm_property_info */>>> = cValuesOf(prop_1_1.ptr).ptr
             val realmSchemaNew = realm_schema_new(classes, 1.toULong(), classProperties)
+
+            val error = cValue<realm_error_t>()
+            val realmGetLastError = realm_get_last_error(error)
+            assertFalse(realmGetLastError)
+
+            error.useContents {
+                assertEquals(0, kind.code)
+                assertNull(message.data)
+                assertEquals(0.toUInt(), this.error)
+            }
+
+            assertTrue(realm_schema_validate(realmSchemaNew))
+
             val config = realm_config_new()
             val path = cValue<realm_string_t> {
 //                data = "alloc()".cstr.getPointer(MemScope()) alternatively
@@ -86,13 +111,15 @@ class CinteropTest {
                 size = p.length.toULong()
             }
 
-            assertTrue(realm_schema_validate(realmSchemaNew))
             realm_config_set_path(config, path)
             realm_config_set_schema(config, realmSchemaNew)
             realm_config_set_schema_mode(config, realm_schema_mode_e.RLM_SCHEMA_MODE_AUTOMATIC)
             realm_config_set_schema_version(config, 1)
 
-            //TODO call realm_get_schema() to obtain and check value for table_key & col_key
+            val realm: CPointer<realm_t>? = realm_open(config)
+            assertNotNull(realm)
+            val schema: CPointer<realm_schema_t>? = realm_get_schema(realm)
+            assertNotNull(schema)
         }
     }
 
