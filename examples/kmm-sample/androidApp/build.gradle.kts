71cdc40c51f0ce4dc3ecd01d92e29027b60daafa
Claus Rørbech <claus.rorbech@mongodb.com>
2020-11-11 12:24:52 +0100

    Add official kmm-sample project from git@github.com:Kotlin/kmm-sample.git
    Exported with:
     git archive --prefix kmm-sample/ --format tgz -o kmm-sample.tgz 325f92

---
+++ examples/kmm-sample/androidApp/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+}
+group = "com.jetbrains"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+}
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        applicationId = "com.jetbrains.androidApp"
+        minSdkVersion(24)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
---
+++ examples/kmm-sample/build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
+        classpath("com.android.tools.build:gradle:4.0.1")
+    }
+}
+group = "com.jetbrains"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
