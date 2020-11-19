371b2de4f23aff2fb3ff30af17a3e87d265c878f
Claus Rørbech <claus.rorbech@mongodb.com>
2020-11-19 15:29:57 +0100

    Resolve FIXME for kmm-sample dependencies on CI

--- examples/kmm-sample/settings.gradle.kts
+++ examples/kmm-sample/settings.gradle.kts
@@ -1,13 +1,8 @@
-// If you want to run against the local source repository just include the build with
+// If you want to run against the local source repository just include the source projects by
+// reincluding the below
 // includeBuild("../../packages")
-// FIXME MPP-BUILD How to structure example builds? Should we use local build artifacts
-//  (includeBuild) or actually verified published artifacts (mavenLocal). Currently the examples
-//  are only statically analyzed on CI but not actually built.
-//  Postpone decision until we have a CI setup that can actually build the whole app on one node.
-//  For now just compose build to allow applying our 'realm-kotlin' plugin to allow configuring the
-//  project for linting purposes. We could use composite build on a single node to just reuse
-//  already built packages, but we might want to postpone the building the example projects until
-//  after building the actual packages.
+
+// Use local sources for CI builds
 if (System.getenv("JENKINS_HOME") != null) {
     includeBuild("../../packages")
 }
