c273b3e9aee34f0d9c9c1b15d150ff5909e1380e
Nabil Hachicha <nabil.hachicha@mongodb.com>
2021-03-16 16:10:48 +0000

    🧹

--- packages/cinterop/src/jvmCommon/kotlin/io/realm/interop/NotificationCallback.kt
+++ packages/cinterop/src/jvmCommon/kotlin/io/realm/interop/NotificationCallback.kt
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package io.realm.interop;
+package io.realm.interop
 
 /**
  * Internal callback used from JNI to notify RealmResults or RealmObject changes.
