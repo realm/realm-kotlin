145d8369834af5c3cfd8f7c7d1b962cd3abfd713
Christian Melchior <christian@ilios.dk>
2021-05-05 10:47:08 +0200

    Set a specific cmake version

--- packages/cinterop/build.gradle.kts
+++ packages/cinterop/build.gradle.kts
@@ -75,6 +75,7 @@ android {
         // Out externalNativeBuild (outside defaultConfig) does not seem to have correct type for setting cmake arguments
         externalNativeBuild {
             cmake {
+                version = "3.18.1"
                 arguments("-DANDROID_STL=c++_shared")
             }
         }
@@ -82,6 +83,7 @@ android {
     // Inner externalNativeBuild (inside defaultConfig) does not seem to have correct type for setting path
     externalNativeBuild {
         cmake {
+            version = "3.18.1"
             path = project.file("src/jvmCommon/CMakeLists.txt")
         }
     }
