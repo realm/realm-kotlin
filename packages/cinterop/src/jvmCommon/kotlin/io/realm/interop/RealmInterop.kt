76cac2b0f6a21d21908eca4b4ea3de5393b9b102
Christian Melchior <christian@ilios.dk>
2021-05-26 17:17:50 +0200

    Formatting

--- packages/cinterop/src/jvmCommon/kotlin/io/realm/interop/RealmInterop.kt
+++ packages/cinterop/src/jvmCommon/kotlin/io/realm/interop/RealmInterop.kt
@@ -135,7 +135,7 @@ actual object RealmInterop {
     actual fun realm_is_frozen(realm: NativePointer): Boolean {
         return realmc.realm_is_frozen(realm.cptr())
     }
-    
+
     actual fun realm_close(realm: NativePointer) {
         realmc.realm_close((realm as LongPointerWrapper).ptr)
     }
