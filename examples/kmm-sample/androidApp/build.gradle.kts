2ace0739b911184b72a33c8a4bdb9bebe1ee13ac
Nabil Hachicha <nabil.hachicha@mongodb.com>
2021-06-08 14:42:04 +0100

    Update to Kotlin 1.5 and refresh dependencies (#273)
    * Update to Kotlin 1.5 and refresh dependencies
    
    * Update example

--- examples/kmm-sample/androidApp/build.gradle.kts
+++ examples/kmm-sample/androidApp/build.gradle.kts
@@ -54,4 +54,9 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
