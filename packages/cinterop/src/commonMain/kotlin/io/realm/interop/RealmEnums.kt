6d483e306e965177803c2c0876b47cdabfebedc3
Clemente Tort <clementetb@gmail.com>
2021-08-24 13:46:55 +0200

    Remove RealmCoreError

--- packages/cinterop/src/commonMain/kotlin/io/realm/interop/RealmEnums.kt
+++ packages/cinterop/src/commonMain/kotlin/io/realm/interop/RealmEnums.kt
@@ -16,7 +16,6 @@
 
 package io.realm.interop
 
-import io.realm.interop.errors.RealmCoreError
 import io.realm.interop.errors.RealmCoreException
 import io.realm.interop.errors.RealmCoreOtherException
 
@@ -113,14 +112,10 @@ expect enum class ErrorType {
 
 fun errorTypeToThrowable(errorType: ErrorType, message: String?): Throwable {
     return when (errorType) {
-        ErrorType.RLM_ERR_OUT_OF_MEMORY,
-        ErrorType.RLM_ERR_MULTIPLE_SYNC_AGENTS,
-        ErrorType.RLM_ERR_ADDRESS_SPACE_EXHAUSTED,
-        ErrorType.RLM_ERR_MAXIMUM_FILE_SIZE_EXCEEDED,
-        ErrorType.RLM_ERR_UNKNOWN,
-        ErrorType.RLM_ERR_OUT_OF_DISK_SPACE -> RealmCoreError(errorType, message)
-        // Recoverable errors
+        ErrorType.RLM_ERR_NONE -> throw IllegalStateException("Cannot map error type $errorType.")
         ErrorType.RLM_ERR_OTHER_EXCEPTION -> RealmCoreOtherException(message)
+        ErrorType.RLM_ERR_UNKNOWN,
+        ErrorType.RLM_ERR_OUT_OF_MEMORY,
         ErrorType.RLM_ERR_NOT_CLONABLE,
         ErrorType.RLM_ERR_NOT_IN_A_TRANSACTION,
         ErrorType.RLM_ERR_WRONG_THREAD,
@@ -131,15 +126,19 @@ fun errorTypeToThrowable(errorType: ErrorType, message: String?): Throwable {
         ErrorType.RLM_ERR_MISSING_PRIMARY_KEY,
         ErrorType.RLM_ERR_UNEXPECTED_PRIMARY_KEY,
         ErrorType.RLM_ERR_WRONG_PRIMARY_KEY_TYPE,
-        ErrorType.RLM_ERR_LOGIC,
         ErrorType.RLM_ERR_MODIFY_PRIMARY_KEY,
         ErrorType.RLM_ERR_READ_ONLY_PROPERTY,
         ErrorType.RLM_ERR_PROPERTY_NOT_NULLABLE,
         ErrorType.RLM_ERR_INVALID_ARGUMENT,
+        ErrorType.RLM_ERR_LOGIC,
         ErrorType.RLM_ERR_NO_SUCH_TABLE,
         ErrorType.RLM_ERR_NO_SUCH_OBJECT,
         ErrorType.RLM_ERR_CROSS_TABLE_LINK_TARGET,
         ErrorType.RLM_ERR_UNSUPPORTED_FILE_FORMAT_VERSION,
+        ErrorType.RLM_ERR_MULTIPLE_SYNC_AGENTS,
+        ErrorType.RLM_ERR_ADDRESS_SPACE_EXHAUSTED,
+        ErrorType.RLM_ERR_MAXIMUM_FILE_SIZE_EXCEEDED,
+        ErrorType.RLM_ERR_OUT_OF_DISK_SPACE,
         ErrorType.RLM_ERR_KEY_NOT_FOUND,
         ErrorType.RLM_ERR_COLUMN_NOT_FOUND,
         ErrorType.RLM_ERR_COLUMN_ALREADY_EXISTS,
@@ -149,8 +148,8 @@ fun errorTypeToThrowable(errorType: ErrorType, message: String?): Throwable {
         ErrorType.RLM_ERR_DUPLICATE_PRIMARY_KEY_VALUE,
         ErrorType.RLM_ERR_INDEX_OUT_OF_BOUNDS,
         ErrorType.RLM_ERR_INVALID_QUERY_STRING,
-        ErrorType.RLM_ERR_CALLBACK,
-        ErrorType.RLM_ERR_INVALID_QUERY -> RealmCoreException(errorType, message)
-        else -> RuntimeException(message)
+        ErrorType.RLM_ERR_INVALID_QUERY,
+        ErrorType.RLM_ERR_CALLBACK -> RealmCoreException(errorType, message)
+        else -> throw IllegalStateException("Error type $errorType not implemented.")
     }
 }
--- packages/cinterop/src/commonMain/kotlin/io/realm/interop/errors/RealmCoreErrors.kt
+++ packages/cinterop/src/commonMain/kotlin/io/realm/interop/errors/RealmCoreErrors.kt
@@ -19,18 +19,9 @@ package io.realm.interop.errors
 import io.realm.interop.ErrorType
 
 /**
- * RealmCoreException represents any recoverable, non-fatal, Realm-Core exception.
+ * Generic representation of a Realm-Core exception.
  */
 class RealmCoreException(val errorType: ErrorType, message: String?) : RuntimeException(message) {
-    override fun toString(): String {
-        return "[$errorType]: $message"
-    }
-}
-
-/**
- * RealmCoreError represents any non-recoverable, fatal, Realm-Core exception.
- */
-class RealmCoreError(val errorType: ErrorType, message: String?) : Error(message) {
     override fun toString(): String {
         return "[$errorType]: $message"
     }
