e75fea438d36acdcf3acb960b8ea6cd4728f2abd
Eduardo López <eduardo.lopez@mongodb.com>
2021-08-24 17:31:29 +0200

    Added one more level of indirection for test module. Now it hosts "local" for all local DB-related tests and will host "sync" for sync-related tests

--- test/settings.gradle.kts
+++ test/settings.gradle.kts
@@ -9,24 +9,26 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 includeBuild("../packages")
 
+include("local")
+
 pluginManagement {
+    plugins {
+    }
     repositories {
-        jcenter()
-        google()
         gradlePluginPortal()
+        google()
     }
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:4.0.1")
+                useModule("com.android.tools.build:gradle:4.1.0")
             }
         }
     }
