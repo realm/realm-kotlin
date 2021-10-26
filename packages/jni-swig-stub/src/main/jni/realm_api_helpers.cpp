65ba7c9d5bed427e697f180b756518c4157c24b8
Eduardo López <eduardo.lopez@mongodb.com>
2021-10-26 15:28:56 +0200

    Removed overkill mapping at cinterop layer

--- packages/jni-swig-stub/src/main/jni/realm_api_helpers.cpp
+++ packages/jni-swig-stub/src/main/jni/realm_api_helpers.cpp
@@ -438,18 +438,7 @@ void set_log_callback(realm_sync_client_config_t* sync_client_config, jobject lo
                                                   static jmethodID log_method = lookup(jenv, "io/realm/internal/interop/SyncLogCallback",
                                                                                        "log",
                                                                                        "(SLjava/lang/String;)V");
-
-                                                  // Convert values not present in the SDK to valid ones
-                                                  int log_level;
-                                                  if (level <= RLM_LOG_LEVEL_TRACE) {
-                                                      log_level = RLM_LOG_LEVEL_ALL;
-                                                  } else if (level == RLM_LOG_LEVEL_DETAIL) {
-                                                      log_level = RLM_LOG_LEVEL_INFO;
-                                                  } else {
-                                                      log_level = level;
-                                                  }
-
-                                                  jenv->CallVoidMethod(log_callback, log_method, log_level, to_jstring(jenv, message));
+                                                  jenv->CallVoidMethod(log_callback, log_method, level, to_jstring(jenv, message));
                                               },
                                               jenv->NewGlobalRef(log_callback), // userdata is the log callback
                                               [](void* userdata) {
