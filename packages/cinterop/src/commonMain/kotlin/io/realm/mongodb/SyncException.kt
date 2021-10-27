bf0d26a6127eaf4e1c03bd2062d8d140d7cb19ae
Eduardo López <eduardo.lopez@mongodb.com>
2021-10-27 14:37:56 +0200

    Added comment on standardised values from enums for cinterop layer

--- packages/cinterop/src/commonMain/kotlin/io/realm/mongodb/SyncException.kt
+++ packages/cinterop/src/commonMain/kotlin/io/realm/mongodb/SyncException.kt
@@ -24,6 +24,7 @@ expect enum class SyncErrorCategory {
     RLM_SYNC_ERROR_CATEGORY_SYSTEM,
     RLM_SYNC_ERROR_CATEGORY_UNKNOWN;
 
+    // TODO Use approach from https://github.com/realm/realm-kotlin/pull/522/files#diff-78c7e4d23c4a144e89ea26c34b8f97ff2111e39db5cdf0f9724deb79f5634194R32 once it gets merged
     companion object {
         fun fromValue(value: Short): SyncErrorCategory
     }
