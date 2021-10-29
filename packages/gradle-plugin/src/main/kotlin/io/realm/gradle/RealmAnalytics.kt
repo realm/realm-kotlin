d3b387874320d1aa4ca04d753cbb380f2a27711c
Christian Melchior <christian@ilios.dk>
2021-10-29 15:37:39 +0200

    Add Sync tracking to analytics (#528)

--- packages/gradle-plugin/src/main/kotlin/io/realm/gradle/RealmAnalytics.kt
+++ packages/gradle-plugin/src/main/kotlin/io/realm/gradle/RealmAnalytics.kt
@@ -19,7 +19,10 @@ package io.realm.gradle
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.execution.TaskExecutionAdapter
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.logging.Logger
 import org.gradle.api.tasks.TaskState
 import java.io.File
 import java.io.FileNotFoundException
@@ -85,12 +88,13 @@ internal class RealmAnalytics : TaskExecutionAdapter() {
             val disableAnalytics: Boolean = project.gradle.startParameter.isOffline || "true".equals(System.getenv()["REALM_DISABLE_ANALYTICS"], ignoreCase = true)
             if (!disableAnalytics) {
                 val json = jsonPayload(project)
-                sendAnalytics(json)
+                sendAnalytics(json, project.logger)
             }
             METRIC_PROCESSED = true
         }
     }
 
+    @Suppress("NestedBlockDepth")
     private fun jsonPayload(project: Project): String {
         val userId = ComputerIdentifierGenerator.get()
         val appId = anonymousAppId(project)
@@ -100,6 +104,26 @@ internal class RealmAnalytics : TaskExecutionAdapter() {
         val projectAndroidExtension: BaseExtension? = project.extensions.findByName("android") as BaseExtension?
         val minSDK = projectAndroidExtension?.defaultConfig?.minSdkVersion?.apiString
         val targetSDK = projectAndroidExtension?.defaultConfig?.targetSdkVersion?.apiString
+
+        // Should be safe to iterate the configurations as we have left the configuration
+        // phase when this is called.
+        var usesSync = false
+        outer@
+        for (conf in project.configurations) {
+            try {
+                for (artifact: ResolvedArtifact in conf.resolvedConfiguration.resolvedArtifacts) {
+                    // In Java we can detect Sync because we enable it through a Gradle closure.
+                    // In Kotlin, this choice is currently determined by which dependency
+                    // people include
+                    if (artifact.name.startsWith("library-sync")) {
+                        usesSync = true
+                        break@outer
+                    }
+                }
+            } catch (ignore: Exception) {
+                // Some artifacts might not be able to resolve, in this case, just ignore them.
+            }
+        }
 
         // FIXME Improve metrics with details about targets, etc.
         //  https://github.com/realm/realm-kotlin/issues/127
@@ -113,6 +137,7 @@ internal class RealmAnalytics : TaskExecutionAdapter() {
                       "Binding": "kotlin",
                       "Language": "kotlin",
                       "Realm Version": "${RealmCompilerSubplugin.version}",
+                      "Sync Enabled": ${if (usesSync) "true" else "false"},
                       "Host OS Type": "$osType",
                       "Host OS Version": "$osVersion",
                       "Target OS Minimum Version": "$minSDK",
@@ -136,17 +161,21 @@ internal class RealmAnalytics : TaskExecutionAdapter() {
     }
 
     @Suppress("TooGenericExceptionCaught")
-    private fun sendAnalytics(json: String) {
+    private fun sendAnalytics(json: String, logger: Logger) {
         try {
+            logger.log(LogLevel.DEBUG, "REALM ANALYTICS: sending payload\n$json")
             val pool = Executors.newSingleThreadExecutor()
             try {
                 pool.execute { networkQuery(json) }
                 pool.awaitTermination(CONNECT_TIMEOUT + READ_TIMEOUT, TimeUnit.MILLISECONDS)
+                logger.log(LogLevel.DEBUG, "REALM ANALYTICS: transfer completed")
             } catch (e: InterruptedException) {
+                logger.log(LogLevel.DEBUG, "REALM ANALYTICS: transfer interrupted")
                 pool.shutdownNow()
             }
         } catch (e: Exception) {
             // Analytics failing for any reason should not crash the build
+            logger.log(LogLevel.DEBUG, "REALM ANALYTICS: transfer failed")
             System.err.println("Could not send analytics: $e")
         }
     }
