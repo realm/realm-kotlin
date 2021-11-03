8345dc25a4cc100783bf23ad7bd6069a3da336a6
Claus Rørbech <claus.rorbech@mongodb.com>
2021-11-03 15:48:16 +0100

    Pass network transport to AppConfiguration instead of constructing it internally

--- packages/library-sync/src/commonMain/kotlin/io/realm/mongodb/internal/AppConfigurationImpl.kt
+++ packages/library-sync/src/commonMain/kotlin/io/realm/mongodb/internal/AppConfigurationImpl.kt
@@ -16,8 +16,6 @@
 
 package io.realm.mongodb.internal
 
-import io.ktor.client.features.logging.Logger
-import io.realm.LogConfiguration
 import io.realm.internal.RealmLog
 import io.realm.internal.interop.NativePointer
 import io.realm.internal.interop.RealmInterop
@@ -29,35 +27,14 @@ import io.realm.internal.platform.RUNTIME
 import io.realm.internal.platform.freeze
 import io.realm.mongodb.AppConfiguration
 import io.realm.mongodb.AppConfiguration.Companion.DEFAULT_BASE_URL
-import kotlinx.coroutines.CoroutineDispatcher
 
 internal class AppConfigurationImpl(
     override val appId: String,
     override val baseUrl: String = DEFAULT_BASE_URL,
-    override val networkTransportDispatcher: CoroutineDispatcher,
+    override val networkTransport: NetworkTransport,
     override val metadataMode: MetadataMode = MetadataMode.RLM_SYNC_CLIENT_METADATA_MODE_PLAINTEXT,
-    logConfig: LogConfiguration,
+    val log: RealmLog
 ) : AppConfiguration {
-
-    val log: RealmLog = RealmLog(configuration = logConfig)
-
-    private val networkTransport: NetworkTransport = KtorNetworkTransport(
-        // FIXME Add AppConfiguration.Builder option to set timeout as a Duration with default \
-        //  constant in AppConfiguration.Companion
-        //  https://github.com/realm/realm-kotlin/issues/408
-        timeoutMs = 5000,
-        dispatcher = networkTransportDispatcher,
-        logger = object : Logger {
-            // This should ideally be the AppConfigurationImpl.log but iOS ktor client seems to
-            // pass all configuration to another thread which freezes it. Having two logs with the
-            // same configuration is not ideal but would yield the same result when the
-            // configuration and loggers itself are frozen.
-            val log: RealmLog = RealmLog(configuration = logConfig)
-            override fun log(message: String) {
-                this.log.debug(message)
-            }
-        }
-    ).freeze() // Kotlin network client needs to be frozen before passed to the C-API
 
     // Only freeze anything after all properties are setup as this triggers freezing the actual
     // AppConfigurationImpl instance itself
