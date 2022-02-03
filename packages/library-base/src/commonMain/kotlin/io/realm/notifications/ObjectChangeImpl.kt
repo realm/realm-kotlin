7427d4819728b7dbacb9d1fe1c20c87d76408ed0
Clemente Tort <clementetb@gmail.com>
2022-02-03 15:41:09 +0100

    Revert "Use data classes"
    This reverts commit 76dda7ebec6462a189be14f07c4756b9107cfcfa.

--- packages/library-base/src/commonMain/kotlin/io/realm/notifications/ObjectChangeImpl.kt
+++ packages/library-base/src/commonMain/kotlin/io/realm/notifications/ObjectChangeImpl.kt
@@ -2,12 +2,12 @@ package io.realm.notifications
 
 import io.realm.RealmObject
 
-internal data class InitialObjectImpl<O : RealmObject>(override val obj: O) : InitialObject<O> {
+internal class InitialObjectImpl<O : RealmObject>(override val obj: O) : InitialObject<O> {
     override val state: ObjectChange.State
         get() = ObjectChange.State.INITIAL
 }
 
-internal data class UpdatedObjectImpl<O : RealmObject>(
+internal class UpdatedObjectImpl<O : RealmObject>(
     override val obj: O,
     override val changedFields: Array<String>
 ) : UpdatedObject<O> {
@@ -15,7 +15,7 @@ internal data class UpdatedObjectImpl<O : RealmObject>(
         get() = ObjectChange.State.UPDATED
 }
 
-internal data class DeletedObjectImpl<O : RealmObject> : DeletedObject<O> {
+internal class DeletedObjectImpl<O : RealmObject> : DeletedObject<O> {
     override val state: ObjectChange.State
         get() = ObjectChange.State.DELETED
 
