6d7b727af7be6026238788297c347e707cc1213a
Clemente Tort <clementetb@gmail.com>
2022-02-07 15:50:50 +0100

    Remove state property

--- packages/library-base/src/commonMain/kotlin/io/realm/notifications/RealmChange.kt
+++ packages/library-base/src/commonMain/kotlin/io/realm/notifications/RealmChange.kt
@@ -3,15 +3,11 @@ package io.realm.notifications
 import io.realm.BaseRealm
 
 sealed interface RealmChange<R : BaseRealm> {
-    enum class State {
-        INITIAL,
-        UPDATED
-    }
-    val state: State
     /**
      * Returns the newest version of the Realm.
      */
     val realm: R
 }
+
 interface InitialRealm<R : BaseRealm> : RealmChange<R>
 interface UpdatedRealm<R : BaseRealm> : RealmChange<R>
