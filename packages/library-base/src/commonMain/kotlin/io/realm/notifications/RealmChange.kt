ec4e6430435933e7747d44f26f646364b2f7d531
Clemente Tort <clementetb@gmail.com>
2022-02-09 13:07:32 +0100

    Trim interfaces that are not part of this PR

--- packages/library-base/src/commonMain/kotlin/io/realm/notifications/RealmChange.kt
+++ packages/library-base/src/commonMain/kotlin/io/realm/notifications/RealmChange.kt
@@ -2,12 +2,23 @@ package io.realm.notifications
 
 import io.realm.BaseRealm
 
+/**
+ * A [RealmChange] describes the type of changes that can be observed on a realm.
+ */
 sealed interface RealmChange<R : BaseRealm> {
     /**
      * Returns the newest version of the Realm.
      */
     val realm: R
 }
-
+/**
+ * [InitialRealm] describes the initial event observed on a Realm flow. It contains the Realm instance
+ * it was subscribed to.
+ */
 interface InitialRealm<R : BaseRealm> : RealmChange<R>
+
+/**
+ * [UpdatedRealm] describes a Realm update event to be observed on a Realm flow after the [InitialRealm].
+ * It contains a Realm instance of the updated Realm.
+ */
 interface UpdatedRealm<R : BaseRealm> : RealmChange<R>
