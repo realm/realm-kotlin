c084aea20a86494c61357d0a14c79d0e970dcefb
Nabil Hachicha <nabil.hachicha@mongodb.com>
2022-02-11 12:03:13 +0000

    PR feedback

--- packages/plugin-compiler/src/test/kotlin/io/realm/compiler/GenerationExtensionTest.kt
+++ packages/plugin-compiler/src/test/kotlin/io/realm/compiler/GenerationExtensionTest.kt
@@ -121,16 +121,6 @@ class GenerationExtensionTest {
     }
 
     @Test
-    fun `multiple constructors in model definition`() {
-        val inputs = Files("/multiple_ctor")
-
-        val result = compile(inputs)
-
-        assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
-        inputs.assertGeneratedIR()
-    }
-
-    @Test
     @Suppress("LongMethod")
     fun `synthetic method generated`() {
         val inputs = Files("/sample")
