cb128db72455c71d872cd112aafdd1391f84d183
Nabil Hachicha <nabil.hachicha@mongodb.com>
2022-04-01 12:10:06 +0100

    Using latest Kotlin version to test compatibility for the sample project

--- examples/kmm-sample/build.gradle.kts
+++ examples/kmm-sample/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.latestKotlin}")
         classpath("com.android.tools.build:gradle:${Versions.Android.buildTools}")
         classpath ("io.realm.kotlin:gradle-plugin:${Realm.version}")
     }
