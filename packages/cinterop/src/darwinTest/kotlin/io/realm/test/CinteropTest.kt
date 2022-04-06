7cdd418dce62586c78d214b7099194f2eecd3a7e
Christian Melchior <christian@ilios.dk>
2022-04-06 23:06:51 +0200

    Fix version test

--- packages/cinterop/src/darwinTest/kotlin/io/realm/test/CinteropTest.kt
+++ packages/cinterop/src/darwinTest/kotlin/io/realm/test/CinteropTest.kt
@@ -82,7 +82,7 @@ class CinteropTest {
 
     @Test
     fun version() {
-        assertEquals("11.10.0", realm_get_library_version()!!.toKString())
+        assertEquals("11.13.0", realm_get_library_version()!!.toKString())
     }
 
     @Test
