f9b7dcd88945e7f6405fb8b7547e8adf254d9665
Christian Melchior <christian@ilios.dk>
2022-04-28 17:50:17 +0200

    Fix imports

--- test/sync/src/commonMain/kotlin/io/realm/test/mongodb/TestApp.kt
+++ test/sync/src/commonMain/kotlin/io/realm/test/mongodb/TestApp.kt
@@ -20,7 +20,6 @@ package io.realm.test.mongodb
 
 import io.ktor.client.request.get
 import io.realm.internal.interop.RealmInterop
-import io.realm.internal.platform.appFilesDirectory
 import io.realm.internal.platform.runBlocking
 import io.realm.internal.platform.singleThreadDispatcher
 import io.realm.log.LogLevel
