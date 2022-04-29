06938936c7cc410594d40f947df29f3d9df8f434
Christian Melchior <christian@ilios.dk>
2022-04-29 19:04:07 +0200

    Prepare next dev iteration

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -17,7 +17,7 @@
 
 object Realm {
     val ciBuild = (System.getenv("JENKINS_HOME") != null)
-    const val version = "0.11.0"
+    const val version = "0.11.1-SNAPSHOT"
     const val group = "io.realm.kotlin"
     const val projectUrl = "https://realm.io"
     const val pluginPortalId = "io.realm.kotlin"
