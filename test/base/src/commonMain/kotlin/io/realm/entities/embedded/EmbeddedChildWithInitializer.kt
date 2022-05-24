2d8a9e4e490144f36973d9715206c5cfc3854ae8
Claus Rørbech <claus.rorbech@mongodb.com>
2022-05-24 10:43:17 +0200

    Apply suggestions from code review
    Co-authored-by: Christian Melchior <christian@ilios.dk>

--- test/base/src/commonMain/kotlin/io/realm/entities/embedded/EmbeddedChildWithInitializer.kt
+++ test/base/src/commonMain/kotlin/io/realm/entities/embedded/EmbeddedChildWithInitializer.kt
@@ -3,6 +3,5 @@ package io.realm.entities.embedded
 import io.realm.RealmObject
 
 class EmbeddedChildWithInitializer : RealmObject {
-
     var child: EmbeddedChild? = EmbeddedChild("Initial child")
 }
