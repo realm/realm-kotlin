ddd378bc2eb91e98ca1660c177a5f8550fe65d65
Claus Rørbech <claus.rorbech@mongodb.com>
2022-06-01 15:28:22 +0200

    Another round for linting

--- benchmarks/shared/src/commonMain/kotlin/io/realm/kotlin/benchmarks/Entities.kt
+++ benchmarks/shared/src/commonMain/kotlin/io/realm/kotlin/benchmarks/Entities.kt
@@ -15,11 +15,11 @@
  */
 package io.realm.kotlin.benchmarks
 
+import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
-import io.realm.kotlin.ext.realmListOf
 import kotlin.reflect.KClass
 
 val SCHEMAS = mapOf(
