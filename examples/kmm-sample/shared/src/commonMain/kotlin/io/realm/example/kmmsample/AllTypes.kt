ef7d8cb513bbb1a6fce53f37b5578d745d8b631d
Claus Rørbech <claus.rorbech@mongodb.com>
2022-06-03 13:01:19 +0200

    Another round for linting

--- examples/kmm-sample/shared/src/commonMain/kotlin/io/realm/example/kmmsample/AllTypes.kt
+++ examples/kmm-sample/shared/src/commonMain/kotlin/io/realm/example/kmmsample/AllTypes.kt
@@ -17,12 +17,12 @@
 
 package io.realm.example.kmmsample
 
+import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.types.ObjectId
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
-import io.realm.kotlin.ext.realmListOf
 
 // This class is included to make sure the compiler-plugin can handle various type, given the min/max
 // version of Kotlin this project is compiled against.
