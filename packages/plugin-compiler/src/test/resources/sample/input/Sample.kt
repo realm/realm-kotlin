df52855721ad00763774c64bcd6665a67c89c44d
Eduardo López <eduardo.lopez@mongodb.com>
2022-06-28 12:11:16 +0200

    Fixed compiler tests

--- packages/plugin-compiler/src/test/resources/sample/input/Sample.kt
+++ packages/plugin-compiler/src/test/resources/sample/input/Sample.kt
@@ -16,17 +16,17 @@
 
 package sample.input
 
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.ext.realmSetOf
 import io.realm.kotlin.types.EmbeddedRealmObject
 import io.realm.kotlin.types.ObjectId
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.RealmSet
 import io.realm.kotlin.types.annotations.Ignore
 import io.realm.kotlin.types.annotations.Index
 import io.realm.kotlin.types.annotations.PrimaryKey
-import io.realm.kotlin.ext.realmListOf
-import io.realm.kotlin.types.RealmSet
-import io.realm.kotlin.types.realmSetOf
 import java.util.*
 
 class Sample : RealmObject {
