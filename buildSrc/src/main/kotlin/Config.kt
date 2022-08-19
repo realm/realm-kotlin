a4baa9b39e9e69ae5bc772f595fa09e08a26ff4f
Christian Melchior <christian.melchior@mongodb.com>
2022-08-19 16:44:16 +0200

    Move away from composite actions again

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -16,7 +16,7 @@
  */
 
 object Realm {
-    val ciBuild = (System.getenv("JENKINS_HOME") != null)
+    val ciBuild = (System.getenv("CI") != null)
     const val version = "1.1.0-SNAPSHOT"
     const val group = "io.realm.kotlin"
     const val projectUrl = "https://realm.io"
