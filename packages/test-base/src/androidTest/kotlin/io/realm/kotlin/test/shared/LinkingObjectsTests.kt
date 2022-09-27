60c06ac3c3a8f3060e86f5c418b6e8f53dd94836
Clemente <clemente.tort@mongodb.com>
2022-09-27 18:05:34 +0200

    Add several tests

--- packages/test-base/src/androidTest/kotlin/io/realm/kotlin/test/shared/LinkingObjectsTests.kt
+++ packages/test-base/src/androidTest/kotlin/io/realm/kotlin/test/shared/LinkingObjectsTests.kt
@@ -15,27 +15,38 @@
  */
 
 @file:Suppress("invisible_member", "invisible_reference")
+
 package io.realm.kotlin.test.shared
 
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.entities.Sample
 import io.realm.kotlin.ext.isManaged
 import io.realm.kotlin.ext.linkingObjects
+import io.realm.kotlin.ext.query
 import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.ext.realmSetOf
 import io.realm.kotlin.test.platform.PlatformUtils
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.internal.asDynamicRealm
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.ResultsChange
+import io.realm.kotlin.notifications.UpdatedResults
 import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmSet
+import io.realm.kotlin.types.RealmUUID
+import io.realm.kotlin.types.annotations.Ignore
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.runBlocking
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFails
 import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class Parent : RealmObject {
     var child: Child? = null
@@ -49,71 +60,128 @@ class Child : RealmObject {
     val parentsBySet by linkingObjects(Parent::childSet)
 }
 
-val Child.parentsOrNull: RealmResults<Parent>?
-    get() = if (isManaged()) parents else null
+class Recursive : RealmObject {
+    var name: RealmUUID = RealmUUID.random()
 
-class Recursive(var name: String) : RealmObject {
-    constructor(): this("")
     var recursive: Recursive? = null
     val references by linkingObjects(Recursive::recursive)
+}
+
+class MissingSourceProperty : RealmObject {
+    @Ignore
+    var reference: MissingSourceProperty? = null
+    val references by linkingObjects(MissingSourceProperty::reference)
 }
 
 class LinkingObjectsTests {
     private lateinit var realm: Realm
     private lateinit var tmpDir: String
 
+    private fun Child.assertParents(expectedSize: Int) {
+        assertEquals(expectedSize, parents.size)
+        assertEquals(expectedSize, parentsByList.size)
+        assertEquals(expectedSize, parentsBySet.size)
+    }
+
     @BeforeTest
     fun setup() {
         tmpDir = PlatformUtils.createTempDir()
-        val configuration = RealmConfiguration.Builder(setOf(Parent::class, Child::class, Recursive::class))
-            .directory(tmpDir)
-            .build()
+        val configuration =
+            RealmConfiguration.Builder(setOf(Parent::class, Child::class, Recursive::class))
+                .directory(tmpDir)
+                .build()
 
         realm = Realm.open(configuration)
     }
 
     @Test
-    fun unmanaged_throw() {
-        val a = Child()
+    fun unmanaged_throws() {
+        val child = Child()
+        val parent = Parent()
 
-        assertFails {
-            a.parents
+        parent.child = child
+        parent.childSet = realmSetOf(child)
+        parent.childList = realmListOf(child)
+
+        assertFailsWith<IllegalStateException> {
+            child.parents
         }
 
-        assertNull(a.parentsOrNull)
+        assertFailsWith<IllegalStateException> {
+            child.parentsBySet
+        }
+
+        assertFailsWith<IllegalStateException> {
+            child.parentsByList
+        }
+    }
+
+
+    // TODO not part of the final test suite, its is here to validate that the users can write their own code
+    private val Child.parentsOrNull: RealmResults<Parent>?
+        get() = if (isManaged()) parents else null
+
+    @Test
+    fun validateExtensionMethod() {
+        val child = Child()
+        assertNull(child.parentsOrNull)
     }
 
     @Test
-    fun managed_works() {
+    fun managed_multipleChildren() {
         runBlocking {
-            val child = realm.write {
-                val parent = this.copyToRealm(Parent())
+            realm.write {
                 val child = this.copyToRealm(Child())
 
-                parent.child = child
-                parent.childList.add(child)
-                parent.childSet.add(child)
+                val parents = Array(5) {
+                    this.copyToRealm(Parent())
+                }
 
-                child
+                child.assertParents(0)
+
+                parents.forEach { parent ->
+                    parent.child = child
+                    parent.childList.add(child)
+                    parent.childSet.add(child)
+                }
+
+                child.assertParents(parents.size)
             }
-            assertEquals(1, child.parents.size)
-            assertEquals(1, child.parentsByList.size)
-            assertEquals(1, child.parentsBySet.size)
         }
+    }
 
-        realm.asDynamicRealm().query(Child::class.simpleName!!).first().find()!!
-            .let { dynamicObject ->
-                assertEquals(1, dynamicObject.getLinkingObjects(Child::parents.name).size)
-                assertEquals(1, dynamicObject.getLinkingObjects(Child::parentsByList.name).size)
-                assertEquals(1, dynamicObject.getLinkingObjects(Child::parentsBySet.name).size)
+    @Test
+    fun managed_duplicateChildren() {
+        runBlocking {
+            realm.write {
+                val child = this.copyToRealm(Child())
+
+                val parents = Array(5) {
+                    this.copyToRealm(Parent())
+                }
+
+                child.assertParents(0)
+
+                parents.forEach { parent ->
+                    parent.child = child
+                    parent.childList.add(child)
+                    parent.childList.add(child)
+                    parent.childSet.add(child)
+                    parent.childSet.add(child)
+                }
+
+                assertEquals(5, child.parents.size)
+                assertEquals(10, child.parentsByList.size)
+                assertEquals(5, child.parentsBySet.size)
             }
+        }
     }
 
     @Test
     fun recursive() {
         runBlocking {
             val recursive = realm.write {
-                val recursive = this.copyToRealm(Recursive("hello world"))
+                val recursive = this.copyToRealm(Recursive())
                 recursive.recursive = recursive
                 recursive
             }
@@ -122,23 +190,219 @@ class LinkingObjectsTests {
         }
     }
 
+    // Dynamic tests
     @Test
     fun dynamic() {
         runBlocking {
-            val recursive = realm.write {
-                val recursive = this.copyToRealm(Recursive("hello world"))
-                recursive.recursive = recursive
-                recursive
+            realm.write {
+                val child = this.copyToRealm(Child())
+
+                this.copyToRealm(Parent().apply {
+                    this.child = child
+                    this.childSet.add(child)
+                    this.childList.add(child)
+                })
             }
-            assertEquals(1, recursive.references.size)
-            assertEquals(recursive.name, recursive.references[0].name)
+
+            realm.asDynamicRealm().query(Child::class.simpleName!!).first().find()!!
+                .let { dynamicObject ->
+                    assertEquals(1, dynamicObject.getLinkingObjects(Child::parents.name).size)
+                    assertEquals(1, dynamicObject.getLinkingObjects(Child::parentsByList.name).size)
+                    assertEquals(1, dynamicObject.getLinkingObjects(Child::parentsBySet.name).size)
+                }
+        }
+    }
+
+    @Test
+    fun dynamicMissingProperty_throws() {
+        runBlocking {
+            realm.write {
+                this.copyToRealm(Recursive())
+            }
             realm.asDynamicRealm().let { dynamicRealm ->
                 val child = dynamicRealm.query("Recursive").first().find()!!
                 assertFailsWith<IllegalArgumentException> {
                     child.getLinkingObjects("missing")
                 }
-                assertEquals(1, child.getLinkingObjects("references").size)
             }
         }
+    }
+
+    // Missing stuff
+    @Test
+    fun classNotInSchema() {
+        runBlocking {
+            tmpDir = PlatformUtils.createTempDir()
+            val configuration =
+                RealmConfiguration.Builder(setOf(Child::class))
+                    .directory(tmpDir)
+                    .build()
+
+            assertFailsWith<IllegalStateException> {
+                Realm.open(configuration)
+            }
+        }
+    }
+
+    @Test
+    fun fieldNotInClass() {
+        runBlocking {
+            tmpDir = PlatformUtils.createTempDir()
+            val configuration =
+                RealmConfiguration.Builder(setOf(MissingSourceProperty::class))
+                    .directory(tmpDir)
+                    .build()
+
+            assertFailsWith<IllegalStateException> {
+                Realm.open(configuration)
+            }
+        }
+    }
+
+    @Test
+    fun migration() {
+
+    }
+
+    // Smoke tests
+    @Test
+    fun notifications_objectLink() {
+        runBlocking {
+            val child = realm.write {
+                copyToRealm(Child())
+            }
+
+            val c = Channel<ResultsChange<Parent>>(1)
+            val observer = async {
+                realm.query<Child>()
+                    .first()
+                    .find()!!
+                    .parents
+                    .asFlow()
+                    .collect {
+                        c.trySend(it)
+                    }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<InitialResults<*>>(resultsChange)
+                assertTrue(resultsChange.list.isEmpty())
+            }
+
+            realm.write {
+                repeat(5) {
+                    copyToRealm(Parent()).apply {
+                        this.child = findLatest(child)
+                    }
+                }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<UpdatedResults<*>>(resultsChange)
+                assertEquals(5, resultsChange.list.size)
+            }
+
+            realm.write {
+                delete(findLatest(child)!!)
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<UpdatedResults<*>>(resultsChange)
+                assertEquals(0, resultsChange.list.size)
+            }
+
+            observer.cancel()
+            c.close()
+        }
+    }
+
+
+    @Test
+    fun notifications_lists() {
+        runBlocking {
+            val child = realm.write {
+                copyToRealm(Child())
+            }
+
+            val c = Channel<ResultsChange<Parent>>(1)
+            val observer = async {
+                realm.query<Child>()
+                    .first()
+                    .find()!!
+                    .parentsByList
+                    .asFlow()
+                    .collect {
+                        c.trySend(it)
+                    }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<InitialResults<*>>(resultsChange)
+                assertTrue(resultsChange.list.isEmpty())
+            }
+
+            realm.write {
+                repeat(5) {
+                    copyToRealm(Parent()).apply {
+                        this.childList.add(findLatest(child)!!)
+                    }
+                }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<UpdatedResults<*>>(resultsChange)
+                assertEquals(5, resultsChange.list.size)
+            }
+
+            observer.cancel()
+            c.close()
+        }
+    }
+
+    @Test
+    fun notifications_sets() {
+        runBlocking {
+            val child = realm.write {
+                copyToRealm(Child())
+            }
+
+            val c = Channel<ResultsChange<Parent>>(1)
+            val observer = async {
+                realm.query<Child>()
+                    .first()
+                    .find()!!
+                    .parentsBySet
+                    .asFlow()
+                    .collect {
+                        c.trySend(it)
+                    }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<InitialResults<*>>(resultsChange)
+                assertTrue(resultsChange.list.isEmpty())
+            }
+
+            realm.write {
+                repeat(5) {
+                    copyToRealm(Parent()).apply {
+                        this.childSet.add(findLatest(child)!!)
+                    }
+                }
+            }
+
+            c.receive().let { resultsChange ->
+                assertIs<UpdatedResults<*>>(resultsChange)
+                assertEquals(5, resultsChange.list.size)
+            }
+
+            observer.cancel()
+            c.close()
+        }
+    }
+
+    @Test
+    fun queries() {
+        // Test that we can query the results
     }
 }
