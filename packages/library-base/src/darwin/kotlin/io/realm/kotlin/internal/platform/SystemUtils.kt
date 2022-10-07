92aafab8b5aab8f96b9e4ba0d635719adb05bbb4
Christian Melchior <christian.melchior@mongodb.com>
2022-10-07 14:30:10 +0200

    Formatting

--- packages/library-base/src/darwin/kotlin/io/realm/kotlin/internal/platform/SystemUtils.kt
+++ packages/library-base/src/darwin/kotlin/io/realm/kotlin/internal/platform/SystemUtils.kt
@@ -75,7 +75,7 @@ public actual fun <T> T.freeze(): T = this
 public actual val <T> T.isFrozen: Boolean
     get() = false
 
-public actual fun Any.ensureNeverFrozen(): Unit {
+public actual fun Any.ensureNeverFrozen() {
     /* Do nothing */
 }
 
