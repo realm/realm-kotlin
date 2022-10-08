b6be2b3f6317d03af201979591806afa0e6be770
Christian Melchior <christian.melchior@mongodb.com>
2022-10-08 12:33:39 +0200

    Trying to fix dependency substitution

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -69,7 +69,7 @@ object Versions {
     // Build also tested successfully with 3.21.4 (latest release).
     const val cmake = "3.22.1"
     const val coroutines = "1.6.4" // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
-    const val datetime = "0.3.2" // https://github.com/Kotlin/kotlinx-datetime
+    const val datetime = "0.4.0" // https://github.com/Kotlin/kotlinx-datetime
     const val detektPlugin = "1.19.0-RC1" // https://github.com/detekt/detekt
     const val dokka = "1.6.0" // https://github.com/Kotlin/dokka
     const val gradlePluginPublishPlugin = "0.15.0" // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
@@ -81,11 +81,11 @@ object Versions {
     const val kotlin = "1.7.20" // https://github.com/JetBrains/kotlin and https://kotlinlang.org/docs/releases.html#release-details
     const val latestKotlin = "1.7.20" // https://kotlinlang.org/docs/eap.html#build-details
     const val kotlinCompileTesting = "1.4.9" // https://github.com/tschuchortdev/kotlin-compile-testing
-    const val ktlint = "0.45.2" // https://github.com/pinterest/ktlint
+    const val ktlint = "0.47.1" // https://github.com/pinterest/ktlint
     const val ktor = "1.6.8" // https://github.com/ktorio/ktor
     const val nexusPublishPlugin = "1.1.0" // https://github.com/gradle-nexus/publish-plugin
     const val okio = "3.0.0" // https://square.github.io/okio/#releases
-    const val serialization = "1.3.0" // https://kotlinlang.org/docs/releases.html#release-details
+    const val serialization = "1.4.0" // https://kotlinlang.org/docs/releases.html#release-details
     const val shadowJar =  "6.1.0" // https://mvnrepository.com/artifact/com.github.johnrengelman.shadow/com.github.johnrengelman.shadow.gradle.plugin?repo=gradle-plugins
     const val multidex = "2.0.1" // https://developer.android.com/jetpack/androidx/releases/multidex
 }
