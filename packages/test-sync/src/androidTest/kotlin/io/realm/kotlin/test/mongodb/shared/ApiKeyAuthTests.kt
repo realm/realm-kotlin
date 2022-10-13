6018eacc2f1c5c4206b57698cce33e90df259d95
Jacob Gunnarsson <jacob.gunnarsson@mongodb.com>
2022-10-13 13:23:52 +0200

    replicate tests from Realm-Java

--- packages/test-sync/src/androidTest/kotlin/io/realm/kotlin/test/mongodb/shared/ApiKeyAuthTests.kt
+++ packages/test-sync/src/androidTest/kotlin/io/realm/kotlin/test/mongodb/shared/ApiKeyAuthTests.kt
@@ -2,20 +2,30 @@ package io.realm.kotlin.test.mongodb.shared
 
 import io.realm.kotlin.internal.platform.runBlocking
 import io.realm.kotlin.mongodb.User
+import io.realm.kotlin.mongodb.auth.ApiKeyAuth
+import io.realm.kotlin.mongodb.exceptions.ServiceException
 import io.realm.kotlin.test.mongodb.TEST_APP_PARTITION
 import io.realm.kotlin.test.mongodb.TestApp
+import io.realm.kotlin.types.ObjectId
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class ApiKeyAuthTests {
     private lateinit var app: TestApp
     private lateinit var user: User
+    private lateinit var provider: ApiKeyAuth
     @BeforeTest
     fun setup() {
         app = TestApp(appName = TEST_APP_PARTITION)
         user = app.createUserAndLogin()
+        provider = user.apiKeyAuth
     }
 
     @AfterTest
@@ -25,28 +35,123 @@ class ApiKeyAuthTests {
         }
     }
 
-    // TODO Just for checking JNI. Create proper test
     @Test
     fun create() = runBlocking {
-        val key = user.apiKeyAuth.create("foo")
+        val key = provider.create("foo")
         assertEquals("foo", key.name)
+        assertNotNull(key.value)
+        assertNotNull(key.id)
+        assertTrue(key.enabled)
+    }
+
+    @Test
+    fun create_throwsWithInvalidName(): Unit = runBlocking {
+        // worth creating a more specific exception?
+        assertFailsWith<ServiceException> {
+            provider.create("%s")
+        }
+    }
+
+    @Test
+    fun create_throwsWithNoName(): Unit = runBlocking {
+        // worth creating a more specific exception?
+        assertFailsWith<ServiceException> {
+            provider.create("")
+        }
     }
 
     @Test
     fun fetch() = runBlocking {
-        val key = user.apiKeyAuth.create("foo")
-        val test = user.apiKeyAuth.fetch(key.id)
-        assertEquals("foo", test.name)
+        val key1 = provider.create("foo")
+        val key2 = provider.fetch(key1.id)
+        assertEquals(key1.id, key2.id)
+        assertEquals(key1.name, key2.name)
+        assertNull(key2.value)
+        assertEquals(key1.enabled, key2.enabled)
     }
 
-    // TODO Just for checking JNI. Create proper test
+    @Test
+    fun fetch_nonExistingKeyThrows(): Unit = runBlocking {
+        // worth creating a more specific exception?
+        assertFailsWith<ServiceException> {
+            provider.fetch(ObjectId.create())
+        }
+    }
+
     @Test
     fun fetchAll() = runBlocking {
-        user.apiKeyAuth.create("foo")
-        user.apiKeyAuth.create("bar")
-        user.apiKeyAuth.create("baz")
+        val key1 = provider.create("foo")
+        val key2 = provider.create("bar")
+        val keys = provider.fetchAll()
+        assertEquals(2, keys.size)
+        assertTrue(keys.any { it.id == key1.id })
+        assertTrue(keys.any { it.id == key2.id })
+    }
 
-        val keys = user.apiKeyAuth.fetchAll()
-        assertEquals(3, keys.size)
+    @Test
+    fun delete(): Unit = runBlocking {
+        val key1 = provider.create("foo")
+        assertNotNull(provider.fetch(key1.id))
+        provider.delete(key1.id)
+        assertFailsWith<ServiceException> {
+            provider.fetch(key1.id)
+        }
+    }
+
+    @Test
+    fun delete_nonExisitingKeyThrows(): Unit = runBlocking {
+        // worth creating a more specific exception?
+        assertFailsWith<ServiceException> {
+            provider.fetch(ObjectId.create())
+        }
+    }
+
+    @Test
+    fun enable(): Unit = runBlocking {
+        val key = provider.create("foo")
+        provider.disable(key.id)
+        assertFalse(provider.fetch(key.id).enabled)
+        provider.enable(key.id)
+        assertTrue(provider.fetch(key.id).enabled)
+    }
+
+    @Test
+    fun enable_alreadyEnabled() = runBlocking {
+        val key = provider.create("foo")
+        provider.disable(key.id)
+        assertFalse(provider.fetch(key.id).enabled)
+        provider.enable(key.id)
+        assertTrue(provider.fetch(key.id).enabled)
+        provider.enable(key.id)
+        assertTrue(provider.fetch(key.id).enabled)
+    }
+    @Test
+    fun enable_nonExistingKeyThrows(): Unit = runBlocking {
+        assertFailsWith<ServiceException> {
+            provider.enable(ObjectId.create())
+        }
+    }
+
+    @Test
+    fun disable() = runBlocking {
+        val key = provider.create("foo")
+        provider.disable(key.id)
+        assertFalse(provider.fetch(key.id).enabled)
+    }
+
+    @Test
+    fun disable_alreadyDisabled() = runBlocking {
+        val key = provider.create("foo")
+        provider.disable(key.id)
+        assertFalse(provider.fetch(key.id).enabled)
+        provider.disable(key.id)
+        assertFalse(provider.fetch(key.id).enabled)
+    }
+
+    @Test
+    fun disable_nonExistingKeyThrows(): Unit = runBlocking {
+        assertFailsWith<ServiceException> {
+            provider.disable(ObjectId.create())
+        }
     }
 }
