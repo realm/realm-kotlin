4412fb07399e44175e132657248f60e15505ee42
Clemente <clemente.tort@mongodb.com>
2022-10-13 17:08:42 +0200

    Simplify

--- packages/library-base/src/commonMain/kotlin/io/realm/kotlin/internal/RealmLinkingObjectsImpl.kt
+++ packages/library-base/src/commonMain/kotlin/io/realm/kotlin/internal/RealmLinkingObjectsImpl.kt
@@ -15,21 +15,14 @@
  */
 package io.realm.kotlin.internal
 
-import io.realm.kotlin.internal.interop.Callback
-import io.realm.kotlin.internal.interop.RealmChangesPointer
-import io.realm.kotlin.internal.interop.RealmNotificationTokenPointer
 import io.realm.kotlin.notifications.DeletedObject
 import io.realm.kotlin.notifications.ObjectChange
 import io.realm.kotlin.notifications.ResultsChange
 import io.realm.kotlin.notifications.UpdatedObject
-import io.realm.kotlin.notifications.internal.InitialResultsImpl
-import io.realm.kotlin.notifications.internal.UpdatedResultsImpl
 import io.realm.kotlin.query.RealmQuery
 import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.BaseRealmObject
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.ChannelResult
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combineTransform
@@ -39,52 +32,12 @@ internal class RealmLinkingObjectsImpl<E : BaseRealmObject>(
     val targetObject: RealmObjectReference<*>?,
     val realmResults: RealmResultsImpl<E>,
 ) : AbstractList<E>(),
-    RealmResults<E>,
-    InternalDeleteable by realmResults,
-    Observable<RealmLinkingObjectsImpl<E>, ResultsChange<E>>,
-    RealmStateHolder by realmResults,
-    Flowable<ResultsChange<E>> by realmResults {
-
-    override fun emitFrozenUpdate(
-        frozenRealm: RealmReference,
-        change: RealmChangesPointer,
-        channel: SendChannel<ResultsChange<E>>
-    ): ChannelResult<Unit>? {
-        val frozenResult = freeze(frozenRealm)
-
-        val builder = ListChangeSetBuilderImpl(change)
-
-        return if (builder.isEmpty()) {
-            channel.trySend(InitialResultsImpl(frozenResult))
-        } else {
-            if (targetObject!!.isValid()) {
-                channel.trySend(UpdatedResultsImpl(frozenResult, builder.build()))
-            } else {
-                channel.close()
-                null
-            }
-        }
-    }
+    RealmResults<E> by realmResults,
+    Flowable<ResultsChange<E>> {
 
     override val size: Int by realmResults::size
 
     override fun get(index: Int): E = realmResults[index]
-
-    override fun registerForNotification(callback: Callback<RealmChangesPointer>): RealmNotificationTokenPointer {
-        return realmResults.registerForNotification(callback)
-    }
-
-    override fun freeze(frozenRealm: RealmReference): RealmLinkingObjectsImpl<E> {
-        val frozenResults = realmResults.freeze(frozenRealm)
-        val frozenObject = targetObject?.freeze(frozenRealm)
-        return RealmLinkingObjectsImpl(frozenObject, frozenResults)
-    }
-
-    override fun thaw(liveRealm: RealmReference): RealmLinkingObjectsImpl<E> {
-        val thawedResults = realmResults.thaw(liveRealm)
-        val thawedObject = targetObject?.thaw(liveRealm)
-        return RealmLinkingObjectsImpl(thawedObject, thawedResults)
-    }
 
     override fun query(query: String, vararg args: Any?): RealmQuery<E> =
         realmResults.query(query, *args)
@@ -108,22 +61,19 @@ internal class RealmLinkingObjectsImpl<E : BaseRealmObject>(
      * values, if the object has not been deleted, if not it closes cancels the flow.
      */
     override fun asFlow(): Flow<ResultsChange<E>> {
-        val resultsFlow: Flow<ResultsChange<E>> = targetObject!!.owner.run {
-            checkClosed()
-            owner.registerObserver(this@RealmLinkingObjectsImpl)
-        }
+        val resultsFlow: Flow<ResultsChange<E>> = realmResults.asFlow()
+        val targetFlow: Flow<ObjectChange<*>> = targetObject!!.asFlow()
 
-        val targetFlow: Flow<ObjectChange<*>> = targetObject.asFlow()
+        return targetFlow
             .filter { change: ObjectChange<*> ->
                 change !is UpdatedObject
             }
-
-        return targetFlow.combineTransform(resultsFlow) { objectChange: ObjectChange<*>, resultsChange: ResultsChange<E> ->
-            if (objectChange is DeletedObject) {
-                currentCoroutineContext().cancel()
-            } else {
-                emit(resultsChange)
+            .combineTransform(resultsFlow) { objectChange: ObjectChange<*>, resultsChange: ResultsChange<E> ->
+                if (objectChange is DeletedObject) {
+                    currentCoroutineContext().cancel()
+                } else {
+                    emit(resultsChange)
+                }
             }
-        }
     }
 }
