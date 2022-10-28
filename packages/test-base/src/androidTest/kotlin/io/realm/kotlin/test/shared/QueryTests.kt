860bd6596c8e8c57045635f141bc9892d767e598
Claus Rørbech <claus.rorbech@mongodb.com>
2022-10-28 13:30:14 +0200

    Add missing import

--- packages/test-base/src/androidTest/kotlin/io/realm/kotlin/test/shared/QueryTests.kt
+++ packages/test-base/src/androidTest/kotlin/io/realm/kotlin/test/shared/QueryTests.kt
@@ -58,6 +58,7 @@ import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertIs
