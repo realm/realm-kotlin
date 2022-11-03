f6cd6ca3ff72eaaa06265a03b4c2a9967fa952af
Christian Melchior <christian@ilios.dk>
2022-11-03 11:48:24 +0100

    Increase test timeout (#1104)

--- packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/SyncedRealmTests.kt
+++ packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/SyncedRealmTests.kt
@@ -695,7 +695,7 @@ class SyncedRealmTests {
         // Increment counter asynchronously after download initial data (1)
         val increment1 = async {
             Realm.open(config1).use { realm ->
-                realm.syncSession.downloadAllServerChanges(1.seconds)
+                realm.syncSession.downloadAllServerChanges(30.seconds)
                 realm.write {
                     realm.query<SyncObjectWithAllTypes>()
                         .first()
@@ -711,7 +711,7 @@ class SyncedRealmTests {
         // Increment counter asynchronously after download initial data (2)
         val increment2 = async {
             Realm.open(config2).use { realm ->
-                realm.syncSession.downloadAllServerChanges(1.seconds)
+                realm.syncSession.downloadAllServerChanges(30.seconds)
                 realm.write {
                     realm.query<SyncObjectWithAllTypes>()
                         .first()
