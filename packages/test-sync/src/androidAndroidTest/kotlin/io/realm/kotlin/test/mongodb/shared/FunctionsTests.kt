04e7180602aa01f49ec89764ca39340c0ec17dec
Clemente <clemente.tort@mongodb.com>
2022-11-16 11:37:53 +0100

    Add copyright headers

--- packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/FunctionsTests.kt
+++ packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/FunctionsTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Realm Inc.
+ * Copyright 2022 Realm Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// We use internal apis for testing purposes. No leaks to the public API.
+
+// We use internal serialization APIs for testing purposes. No leaks to the public API.
 @file:OptIn(InternalSerializationApi::class)
 @file:Suppress("invisible_member", "invisible_reference")
 
