265bcee8abed9532d42193f2b60f0bd77dbd93bd
Christian Melchior <christian.melchior@mongodb.com>
2022-12-21 19:40:19 +0100

    Disable test

--- packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/SyncSessionTests.kt
+++ packages/test-sync/src/androidAndroidTest/kotlin/io/realm/kotlin/test/mongodb/shared/SyncSessionTests.kt
@@ -406,6 +406,7 @@ class SyncSessionTests {
      * - Open a Realm with the same partition key as the inserted document
      * - Wait for Sync to fetch the document as a valid RealmObject with the matching ObjectId as a PK
      */
+    @Ignore // Waiting for a  fix for https://github.com/realm/realm-core/issues/6144
     @Test
     fun syncingObjectIdFromMongoDB() {
         val adminApi = app.asTestApp
