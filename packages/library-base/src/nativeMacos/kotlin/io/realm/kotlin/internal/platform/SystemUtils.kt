2061fb7fa2fce0209b2776714520cffd81294732
Christian Melchior <christian.melchior@mongodb.com>
2023-01-11 15:14:22 +0100

    No need to use lazy

--- packages/library-base/src/nativeMacos/kotlin/io/realm/kotlin/internal/platform/SystemUtils.kt
+++ packages/library-base/src/nativeMacos/kotlin/io/realm/kotlin/internal/platform/SystemUtils.kt
@@ -3,11 +3,11 @@ package io.realm.kotlin.internal.platform
 import platform.Foundation.NSProcessInfo
 
 // See // See https://stackoverflow.com/questions/8058151/how-does-system-profiler-retrieve-the-full-mac-hardware-identifier
+public actual val OS_NAME: String = "MacOS"
 // These causes memory mapping rendering MemoryTests to fail, so only initialize them if actually needed
-public actual val OS_NAME: String by lazy { "MacOS" }
 public actual val OS_VERSION: String by lazy { NSProcessInfo.Companion.processInfo().operatingSystemVersionString }
-public actual val DEVICE_MANUFACTURER: String by lazy { "" }
-public actual val DEVICE_MODEL: String by lazy { "" }
+public actual val DEVICE_MANUFACTURER: String = ""
+public actual val DEVICE_MODEL: String = ""
 
 @Suppress("FunctionOnlyReturningConstant")
 public actual fun appFilesDirectory(): String {
