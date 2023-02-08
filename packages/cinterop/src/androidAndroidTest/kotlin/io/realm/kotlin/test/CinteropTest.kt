f5e68ab9bfbb3a92bef9a8fc102aa13ea95a24c4
Eduardo López <eduardo.lopez@mongodb.com>
2023-02-08 08:52:06 +0100

    updated core version tests

--- packages/cinterop/src/androidAndroidTest/kotlin/io/realm/kotlin/test/CinteropTest.kt
+++ packages/cinterop/src/androidAndroidTest/kotlin/io/realm/kotlin/test/CinteropTest.kt
@@ -61,7 +61,7 @@ class CinteropTest {
 
     @Test
     fun version() {
-        assertEquals("13.2.0", realmc.realm_get_library_version())
+        assertEquals("13.4.0", realmc.realm_get_library_version())
     }
 
     // Test various schema migration with automatic flag:
