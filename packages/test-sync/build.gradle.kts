e097430b53c850760cd75a33af867c0ba0caf3b4
Claus Rørbech <claus.rorbech@mongodb.com>
2023-06-09 16:23:16 +0200

    Add compiler options to native tests

--- packages/test-sync/build.gradle.kts
+++ packages/test-sync/build.gradle.kts
@@ -116,10 +116,14 @@ kotlin {
         }
     }
 
+    // All kotlin compilation tasks
+    tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class.java).all {
+        kotlinOptions.freeCompilerArgs += listOf( "-P", "plugin:io.realm.kotlin:bundleId=TEST_BUNDLE_ID", )
+    }
+    // JVM specific KotlinCompilation tasks
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
         kotlinOptions.jvmTarget = Versions.jvmTarget
         kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
-        kotlinOptions.freeCompilerArgs += listOf( "-P", "plugin:io.realm.kotlin:bundleId=TEST_BUNDLE_ID", )
     }
 }
 
