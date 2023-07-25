145be39715208d4ccf0f64ac8a56123165759b8d
Claus Rørbech <claus.rorbech@mongodb.com>
2023-07-25 10:42:08 +0200

    Verified script execution in development environment

--- tools/fix-h1s.sh
+++ tools/fix-h1s.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -ex
 
+usage() {
+cat <<EOF
+Usage: $0 <root-dokka-dir>
+EOF
+}
+
+if [ "$#" -ne 1 ] ; then
+  usage
+  exit 1
+fi
+
 # Assume Dokka has been run
-pushd "`dirname "$0"`"/../docs/html
+pushd $1
 
 # Make the output SEO friendly by converting the "h2" title to the proper "h1".
 # For Dokka, this is only an issue on the index page (apparently).
