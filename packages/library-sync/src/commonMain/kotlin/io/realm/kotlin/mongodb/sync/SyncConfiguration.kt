830b4aab24cb6db1f1d9bd0934964682d26fb1c7
Claus Rørbech <claus.rorbech@mongodb.com>
2023-08-03 09:51:11 +0200

    Fix sync compiler error

--- packages/library-sync/src/commonMain/kotlin/io/realm/kotlin/mongodb/sync/SyncConfiguration.kt
+++ packages/library-sync/src/commonMain/kotlin/io/realm/kotlin/mongodb/sync/SyncConfiguration.kt
@@ -568,7 +568,7 @@ public interface SyncConfiguration : Configuration {
                 encryptionKey,
                 compactOnLaunchCallback,
                 null, // migration is not relevant for sync,
-                automaticBacklinkHandling,
+                false, // automatic backlink handling is not relevant for sync
                 initialDataCallback,
                 partitionValue == null,
                 inMemory,
