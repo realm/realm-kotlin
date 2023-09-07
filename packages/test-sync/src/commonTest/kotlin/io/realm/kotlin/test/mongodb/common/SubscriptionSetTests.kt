d25f0f5acc8a2b98b1e0c0f0daff65f32bbe854e
Christian Melchior <christian.melchior@mongodb.com>
2023-09-07 13:41:09 +0200

    Update BAAS test server version

--- packages/test-sync/src/commonTest/kotlin/io/realm/kotlin/test/mongodb/common/SubscriptionSetTests.kt
+++ packages/test-sync/src/commonTest/kotlin/io/realm/kotlin/test/mongodb/common/SubscriptionSetTests.kt
@@ -192,7 +192,10 @@ class SubscriptionSetTests {
         assertFailsWith<BadFlexibleSyncQueryException> {
             subscriptions.waitForSynchronization()
         }
-        assertTrue(subscriptions.errorMessage!!.contains("Client provided query with bad syntax"))
+        assertTrue(
+            subscriptions.errorMessage!!.contains("Invalid query: invalid RQL for table \"FlexParentObject\": syntax error: unexpected Limit, expecting Or or RightParenthesis"),
+            subscriptions.errorMessage
+        )
         subscriptions.update {
             removeAll()
         }
@@ -258,7 +261,10 @@ class SubscriptionSetTests {
             updatedSubs.waitForSynchronization()
         }
         assertEquals(SubscriptionSetState.ERROR, updatedSubs.state)
-        assertTrue(updatedSubs.errorMessage!!.contains("Client provided query with bad syntax"))
+        assertTrue(
+            updatedSubs.errorMessage!!.contains("Invalid query: invalid RQL for table \"FlexParentObject\": syntax error: unexpected Limit, expecting Or or RightParenthesis"),
+            updatedSubs.errorMessage
+        )
     }
 
     // Test case for https://github.com/realm/realm-core/issues/5504
@@ -272,7 +278,10 @@ class SubscriptionSetTests {
         }
         assertEquals(SubscriptionSetState.ERROR, updatedSubs.state)
         assertEquals("TRUEPREDICATE and TRUEPREDICATE LIMIT(1)", updatedSubs.first().queryDescription)
-        assertTrue(updatedSubs.errorMessage!!.contains("Client provided query with bad syntax"))
+        assertTrue(
+            updatedSubs.errorMessage!!.contains("Invalid query: invalid RQL for table \"FlexParentObject\": syntax error: unexpected Limit, expecting Or or RightParenthesis"),
+            updatedSubs.errorMessage
+        )
     }
 
     @Test
