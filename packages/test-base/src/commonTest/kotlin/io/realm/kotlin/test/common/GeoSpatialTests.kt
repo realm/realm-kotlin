a88bb461de539987541cc9ff3c5c5658fffc2505
clementetb <clementetb@users.noreply.github.com>
2023-11-17 14:43:55 +0100

    Add geo types sync integration tests. (#1533)

--- packages/test-base/src/commonTest/kotlin/io/realm/kotlin/test/common/GeoSpatialTests.kt
+++ packages/test-base/src/commonTest/kotlin/io/realm/kotlin/test/common/GeoSpatialTests.kt
@@ -20,18 +20,15 @@ package io.realm.kotlin.test.common
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.annotations.ExperimentalGeoSpatialApi
+import io.realm.kotlin.entities.Location
+import io.realm.kotlin.entities.Restaurant
 import io.realm.kotlin.ext.degrees
 import io.realm.kotlin.ext.km
 import io.realm.kotlin.ext.miles
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.ext.radians
-import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.test.common.utils.assertFailsWithMessage
 import io.realm.kotlin.test.platform.PlatformUtils
-import io.realm.kotlin.types.EmbeddedRealmObject
-import io.realm.kotlin.types.RealmList
-import io.realm.kotlin.types.RealmObject
-import io.realm.kotlin.types.annotations.Ignore
 import io.realm.kotlin.types.geo.Distance
 import io.realm.kotlin.types.geo.GeoBox
 import io.realm.kotlin.types.geo.GeoCircle
@@ -43,41 +40,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.fail
-
-class Restaurant : RealmObject {
-    var location: Location? = null
-}
-
-// Custom embedded model class for storing GeoPoints in Realm.
-class Location : EmbeddedRealmObject {
-    constructor(latitude: Double, longitude: Double) {
-        coordinates.apply {
-            add(longitude)
-            add(latitude)
-        }
-    }
-    constructor() : this(0.0, 0.0) // Empty constructor required by Realm. Should not be used.
-
-    // Name and type required by Realm
-    var coordinates: RealmList<Double> = realmListOf()
-
-    // Name and type by Realm
-    private var type: String = "Point"
-
-    @Ignore
-    var latitude: Double
-        get() = coordinates[1]
-        set(value) {
-            coordinates[1] = value
-        }
-
-    @Ignore
-    var longitude: Double
-        get() = coordinates[0]
-        set(value) {
-            coordinates[0] = value
-        }
-}
 
 @OptIn(ExperimentalGeoSpatialApi::class)
 class GeoSpatialTests {
