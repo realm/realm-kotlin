7a75e3dba39809538ecdd8f5b0f8f203ff344d03
Claus Rørbech <claus.rorbech@gmail.com>
2024-05-21 11:27:10 +0200

    Another round for linting

--- packages/test-sync/src/commonTest/kotlin/io/realm/kotlin/test/mongodb/common/ProgressListenerTests.kt
+++ packages/test-sync/src/commonTest/kotlin/io/realm/kotlin/test/mongodb/common/ProgressListenerTests.kt
@@ -54,7 +54,6 @@ import kotlinx.coroutines.withTimeout
 import org.mongodb.kbson.ObjectId
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
