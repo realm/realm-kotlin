1df43eae631dc109d6d06f8610c5ca91b5705a20
Clemente <clemente.tort@mongodb.com>
2024-06-10 10:11:44 +0200

    TEST

--- packages/library-base/src/commonMain/kotlin/io/realm/kotlin/internal/RealmImpl.kt
+++ packages/library-base/src/commonMain/kotlin/io/realm/kotlin/internal/RealmImpl.kt
@@ -106,6 +106,10 @@ public class RealmImpl private constructor(
     @OptIn(ExperimentalStdlibApi::class)
     public var syncContext: AtomicRef<AutoCloseable?> = atomic(null)
 
+    internal fun test(){
+        
+    }
+
     init {
         @Suppress("TooGenericExceptionCaught")
         // Track whether or not the file was created as part of opening the Realm. We need this
