751a9c1d5f20e9fcb023d947d180b35635e20c11
Nabil Hachicha <nabil.hachicha@gmail.com>
2024-09-15 11:53:02 +0100

    Bump version & update compiler plugin for Sync

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -62,7 +62,7 @@ val HOST_OS: OperatingSystem = findHostOs()
 
 object Realm {
     val ciBuild = (System.getenv("CI") != null)
-    const val version = "2.2.0"
+    const val version = "2.3.0"
     const val group = "io.realm.kotlin"
     const val projectUrl = "https://realm.io"
     const val pluginPortalId = "io.realm.kotlin"
