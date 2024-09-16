3d77cb25fd803c20682ff8ea99b9efda5e69b693
Clemente <clemente.tort@mongodb.com>
2024-09-16 13:16:10 +0200

    Remove sync packages

--- packages/library-sync/src/jvmMain/kotlin/io/realm/kotlin/mongodb/internal/NetworkStateObserver.kt
+++
@@ -1,6 +0,0 @@
-package io.realm.kotlin.mongodb.internal
-
-internal actual fun registerSystemNetworkObserver() {
-    // Do nothing on JVM.
-    // There isn't a great way to detect network connectivity on this platform.
-}
--- packages/test-sync/src/jvmMain/kotlin/io/realm/kotlin/test/mongodb/BaasUtils.kt
+++
@@ -1,3 +0,0 @@
-package io.realm.kotlin.test.mongodb
-
-actual fun baasTestUrl(): String = SyncServerConfig.url
